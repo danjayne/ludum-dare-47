52ed9405b1791a728ca8f40b3453bc7304b54a58
danjayne <csdjayne.otl@gmail.com>
2020-10-03 15:40:23 +0100

    Added Pause Menu + Main Menu Title text

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    private void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    private void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void ButtonPressResumeGame()
+    {
+        ResumeGame();
+    }
+
+    public void ButtonPressMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ButtonPressQuitGame()
+    {
+        Application.Quit();
+    }
+}
