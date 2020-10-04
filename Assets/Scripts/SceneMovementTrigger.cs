cea1516896f545c9798c34abcf5191c255e08c03
danjayne <csdjayne.otl@gmail.com>
2020-10-04 11:10:52 +0100

    Potentially fixed scene transitions. Haven't tried to break it.

--- Assets/Scripts/SceneMovementTrigger.cs
+++ Assets/Scripts/SceneMovementTrigger.cs
@@ -1,4 +1,5 @@
 ﻿using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,17 +19,42 @@ public class SceneMovementTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //var bc = GetComponent<BoxCollider2D>();
+
+        Debug.Log($"Entered Trigger at x position {collision.transform.position.x}");
+
+        //if (collision.transform.position.x < bc.bounds.center.x)
+        //{
+        //    //_vCam.Follow = RightScene;
+        //    //_player.position.Set(bc.bounds.max.x + 1, _player.position.y, _player.position.z);
+        //}
+        //else if (collision.transform.position.x > bc.bounds.center.x)
+        //{
+        //    //_vCam.Follow = LeftScene;
+        //    //_player.position.Set(bc.bounds.min.x - 1, _player.position.y, _player.position.z);
+        //}
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
         var bc = GetComponent<BoxCollider2D>();
 
-        if (collision.transform.position.x < bc.bounds.center.x)
+        Debug.Log($"Entered Trigger at x position {collision.transform.position.x}");
+
+        if (collision.transform.position.x > bc.bounds.max.x)
         {
             _vCam.Follow = RightScene;
-            _player.position.Set(bc.bounds.max.x + 1, _player.position.y, _player.position.z);
+            //_player.position.Set(bc.bounds.max.x + 1, _player.position.y, _player.position.z);
         }
-        else
+        else if (collision.transform.position.x < bc.bounds.min.x)
         {
             _vCam.Follow = LeftScene;
-            _player.position.Set(bc.bounds.min.x - 1, _player.position.y, _player.position.z);
+            //_player.position.Set(bc.bounds.min.x - 1, _player.position.y, _player.position.z);
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
     }
 }
