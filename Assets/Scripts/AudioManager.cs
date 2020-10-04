8ff9beffb80cf6559c3701cb4df1c8b36d932c44
danjayne <csdjayne.otl@gmail.com>
2020-10-04 02:55:14 +0100

    Experimented with Bkg Music + Images on menu screens. Pause menu is broken :S

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region "Singleton"
+
+    private static AudioManager _instance;
+
+    public static AudioManager Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    #endregion
+
+    private AudioSource _audioSource;
+    
+}
