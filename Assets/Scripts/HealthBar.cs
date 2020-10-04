2e43df3f258b43a5706cc527b32184370db23d7a
danjayne <csdjayne.otl@gmail.com>
2020-10-04 18:54:04 +0100

    Health bar test

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Gradient Gradient;
+    private Slider _Slider;
+    public Image _Fill;
+
+    private void Awake()
+    {
+        _Slider = GetComponent<Slider>();
+    }
+
+    public void SetMaxHealth(int maxHealth)
+    {
+        _Slider.maxValue = maxHealth;
+        _Slider.value = maxHealth;
+
+        _Fill.color = Gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int health)
+    {
+        _Slider.value = health;
+
+        _Fill.color = Gradient.Evaluate(_Slider.normalizedValue);
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public HealthBar HealthBar;
+
+    private int CurrentHealth = 0;
+    private Animator _Animator;
+
+    private void Awake()
+    {
+        _Animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        CurrentHealth = MaxHealth;
+        HealthBar.SetMaxHealth(MaxHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CurrentHealth -= damage;
+
+        _Animator.SetTrigger("Hurt");
+
+        if (CurrentHealth <= 0)
+        {
+            Die(); // or game over
+        }
+        else
+        {
+            HealthBar.SetHealth(CurrentHealth);
+        }
+    }
+
+    private void Die()
+    {
+        HealthBar.SetHealth(0);
+        _Animator.SetBool("IsDead", true);
+    }
+}
