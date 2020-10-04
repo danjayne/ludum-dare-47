1d8cd9e31eefdad5ba324ab6fa75c03a6fe59a0f
danjayne <csdjayne.otl@gmail.com>
2020-10-04 17:48:29 +0100

    Fixed an issue where different resolutions resulted in the player seeing outside of the game world

---
+++ Assets/Scripts/ScreenWidthManager.cs
@@ -0,0 +1,36 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWidthManager : MonoBehaviour
+{
+    #region "Singleton"
+
+    private static ScreenWidthManager _instance;
+
+    public static ScreenWidthManager Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    #endregion
+
+    public SpriteRenderer FirstSceneBackground;
+    public CinemachineVirtualCamera _vCam;
+
+    private void Start()
+    {
+        _vCam.Follow = FirstSceneBackground.transform;
+        _vCam.m_Lens.OrthographicSize = (FirstSceneBackground.bounds.size.x * Screen.height) / Screen.width * 0.5f;
+    }
+}
