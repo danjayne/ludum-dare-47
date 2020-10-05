6ec6ccae69169f99891029c9869d9ce3039e410f
danjayne <csdjayne.otl@gmail.com>
2020-10-05 19:47:41 +0100

    Got the bare bones of the crappy intro sequence working

---
+++ Assets/Scripts/StoryboardCanvas.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class StoryboardCanvas : MonoBehaviour
+{
+    public RectTransform FirstPanel;
+    public RectTransform CurrentPanel;
+
+    private void Start()
+    {
+        FirstPanel.gameObject.SetActive(true);
+        CurrentPanel = FirstPanel;
+    }
+
+    public void NextScene()
+    {
+        var i = CurrentPanel.name.Split(new string[] { "Board" }, System.StringSplitOptions.RemoveEmptyEntries)[0];
+        var nextPanel = gameObject.transform.Find($"Board{int.Parse(i) + 1}");
+
+        if (nextPanel == null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            return;
+        }
+
+        CurrentPanel.gameObject.SetActive(false);        
+        CurrentPanel = nextPanel.GetComponent<RectTransform>();
+        CurrentPanel.gameObject.SetActive(true);
+    }
+}
