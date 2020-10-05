3bd3a11538e2b017dabce5ea16c8290dfc3df272
danjayne <csdjayne.otl@gmail.com>
2020-10-05 03:36:47 +0100

    Added A* project + basic AI Path Finding

--- Assets/Scripts/StandardEnemy.cs
+++ Assets/Scripts/StandardEnemy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pathfinding;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,15 +10,35 @@ public class StandardEnemy : MonoBehaviour
 
     int _currentHealth;
     Animator _animator;
+    AIPath _aiPath;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _aiPath = GetComponent<AIPath>();
     }
 
     private void Start()
     {
         _currentHealth = MaxHealth;
+    }
+
+    private void Update()
+    {
+        if (_aiPath != null)
+            RotateEnemyBasedOnAIPath();
+    }
+
+    private void RotateEnemyBasedOnAIPath()
+    {
+        if (_aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (_aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 
     public void TakeDamage(int damage)
