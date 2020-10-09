dea04541d584f25d761639471b4d581b31c73b94
danjayne <csdjayne.otl@gmail.com>
2020-10-10 00:49:10 +0100

    GrabHand test

---
+++ Assets/Scripts/GrabHand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabHand : MonoBehaviour
+{
+    public Animator Animator;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Animator.SetBool("IsGrabbing", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+            Animator.SetBool("IsGrabbing", false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+    }
+}
